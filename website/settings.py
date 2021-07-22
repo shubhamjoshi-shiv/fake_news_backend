9748ceaba68f0096a9562bc5fbdc1505f58a6b4f
shiv <shubhamjoshi.shiv@gmail.com>
2021-07-22 22:42:27 +0530

    static file ka rrot change kiya sara

--- website/settings.py
+++ website/settings.py
@@ -126,6 +126,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = os.path.normpath(os.path.join(BASE_DIR, 'static'))
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
