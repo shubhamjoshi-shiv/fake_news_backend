c56b9c477c4cc0eee83e1468b81d381dab673739
shiv <shubhamjoshi.shiv@gmail.com>
2021-07-22 22:48:24 +0530

    include django heroku

--- website/settings.py
+++ website/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
 
@@ -129,3 +130,6 @@ STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
